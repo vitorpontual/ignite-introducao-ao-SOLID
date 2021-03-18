68cb735de8cb2320951870114e7e83368573420b
Vitor Pontual <vitorpguedes@me.com>
2021-03-18 19:46:55 -0300

    Desafio Ignite

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    if (Array.isArray(user_id)) {
+      throw Error("its not array");
+    }
+
+    try {
+      const all = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(all);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
